1e48e086dca0ae5ae99d1f6ff10cda94d2e2b8c7
Christophe van Baalen <chichoch@gmail.com>
2019-10-27 13:13:22 +0100

    Implemented Submit/show Poll

--- src/firebase.js
+++ src/firebase.js
@@ -1,4 +1,4 @@
-import * as firebase from 'firebase';
+import * as firebase from "firebase";
 
 const config = {
   apiKey: "AIzaSyDfgdwAfThtFkMsgbYZJzG4tJeOqWQD3fE",
@@ -9,9 +9,22 @@ const config = {
   messagingSenderId: "571568256217",
   appId: "1:571568256217:web:3a2c1766b3f3680c2a3ac8",
   measurementId: "G-39S0LFNT8Q"
-}
+};
+
 firebase.initializeApp(config);
 
 const databaseRef = firebase.database().ref();
 
-export const postRef = databaseRef.child('posts');
+export const pollsRef = databaseRef.child("polls");
+
+/**
+ * A Poll is defined as follows:
+ * {
+ *    question: String
+ *    alternatives: Array<String>
+ * }
+ * @param {Poll} poll
+ */
+export const submitPoll = poll => {
+  pollsRef.push().set(poll);
+};
