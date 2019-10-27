5433bcad09a31782d77723f2f7da190765ff0b92
Christophe van Baalen <chichoch@gmail.com>
2019-10-27 11:50:08 +0100

    Added firebase basics

---
+++ src/firebase.js
@@ -0,0 +1,17 @@
+import * as firebase from 'firebase';
+
+const config = {
+  apiKey: "AIzaSyDfgdwAfThtFkMsgbYZJzG4tJeOqWQD3fE",
+  authDomain: "castdotvote.firebaseapp.com",
+  databaseURL: "https://castdotvote.firebaseio.com",
+  projectId: "castdotvote",
+  storageBucket: "castdotvote.appspot.com",
+  messagingSenderId: "571568256217",
+  appId: "1:571568256217:web:3a2c1766b3f3680c2a3ac8",
+  measurementId: "G-39S0LFNT8Q"
+}
+firebase.initializeApp(config);
+
+const databaseRef = firebase.database().ref();
+
+export const postRef = databaseRef.child('posts');
